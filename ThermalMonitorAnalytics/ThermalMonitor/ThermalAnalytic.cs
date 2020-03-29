706d02e4f0d8f4f4a9d3ec7d317a10e2584e19e0
knoxaramav2 <wtscott@iastate.edu>
2020-03-28 22:42:29 -0500

    Added projects

---
+++ ThermalMonitorAnalytics/ThermalMonitor/ThermalAnalytic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThermalMonitor
+{
+    public partial class ThermalAnalytic : ServiceBase
+    {
+        #region public methods
+        public ThermalAnalytic()
+        {
+            InitializeComponent();
+            RegisterLogs();
+            
+        }
+
+        #endregion
+
+        #region setup
+        private void RegisterLogs()
+        {
+            reportLog = new EventLog();
+        }
+
+        #endregion
+
+
+        #region events
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+        #endregion
+
+    }
+}
